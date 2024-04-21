2200430cff4970c668ab211c6654f295310be64e
max5961 <mmorrissey5961@gmail.com>
2024-04-21 18:24:00 -0400

    Clean up MultipleChoice component

--- src/Components/quizMode/MultipleChoice.tsx
+++ src/Components/quizMode/MultipleChoice.tsx
@@ -5,37 +5,137 @@ import { Box, Text } from "ink";
 import { MC } from "../../interfaces.js";
 import { HorizontalLine } from "../Lines.js";
 
+export function MultipleChoice({
+    question,
+}: {
+    question: MC;
+}): React.ReactElement {
+    const [currIndex, setCurrIndex] = useState<number>(0);
+
+    useInput((input, key) => {
+        // move down MC
+        if (input === "j" || key.downArrow) {
+            if (currIndex === question.choices.length - 1) {
+                return;
+            } else {
+                setCurrIndex(currIndex + 1);
+            }
+        }
+
+        // move up MC
+        if (input === "k" || key.upArrow) {
+            if (currIndex === 0) {
+                return;
+            } else {
+                setCurrIndex(currIndex - 1);
+            }
+        }
+
+        // these keybinds are handled by the Quiz component for next/prev
+        // question, but this also ensures that if the next question is Multiple
+        // Choice, the index will be set to 0, and the flashing correct/incorrect
+        // interval will also be halted
+        if (
+            input === "n" ||
+            input === "h" ||
+            key.rightArrow ||
+            input === "b" ||
+            input === "h" ||
+            key.leftArrow
+        ) {
+            setCurrIndex(0);
+        }
+    });
+
+    return (
+        <Box justifyContent="center" flexDirection="column">
+            <Box alignSelf="center">
+                <Text color="yellow" dimColor>
+                    {question.q}
+                </Text>
+            </Box>
+            <HorizontalLine />
+            <MCList
+                choices={question.choices}
+                currIndex={currIndex}
+                correctAnswer={question.a}
+            />
+        </Box>
+    );
+}
+
 function MCList({
     choices,
     currIndex,
-    highlightCorrect,
     correctAnswer,
 }: {
     choices: { [key: string]: string }[];
     currIndex: number;
-    highlightCorrect: boolean;
     correctAnswer: string;
 }): React.ReactElement {
+    const [highlightAnswer, setHighlightAnswer] = useState<boolean>(false);
+    const [highlightChoice, setHighlightChoice] = useState<boolean>(false);
+    const [timeoutId, setTimeoutId] = useState<ReturnType<
+        typeof setTimeout
+    > | null>(null);
+
+    function highlight(
+        setHighlight: (b: boolean) => void,
+    ): ReturnType<typeof setTimeout> {
+        setHighlight(true);
+
+        return setTimeout(() => {
+            setHighlight(false);
+        }, 1000);
+    }
+
+    useInput((input, key) => {
+        if (timeoutId) {
+            setHighlightChoice(false);
+            setHighlightAnswer(false);
+        }
+
+        if (input === "a") {
+            setTimeoutId(highlight(setHighlightAnswer));
+        }
+
+        if (input === "c" || key.return) {
+            setTimeoutId(highlight(setHighlightChoice));
+        }
+    });
+
     return (
         <Box flexDirection="column" flexGrow={1}>
             {choices.map((choice: { [key: string]: string }, index: number) => {
+                // The JSON format is a nuisance here, but the format should make
+                // writing the JSON itself easier which is of greater importance
                 const choiceMarker = Object.keys(choice)[0];
                 const choiceTextContent = choice[choiceMarker];
 
+                // highlight current selected choice
                 let color: string = "white";
                 if (index === currIndex) {
                     color = "blue";
                 }
 
                 let isInverse: boolean = false;
-                if (highlightCorrect) {
-                    if (choiceMarker === correctAnswer) {
-                        color = "green";
+
+                // highlight user choice
+                if (highlightChoice) {
+                    if (index === currIndex) {
+                        if (choiceMarker === correctAnswer) {
+                            color = "green";
+                        } else {
+                            color = "red";
+                        }
                         isInverse = true;
                     }
+                }
 
-                    if (choiceMarker !== correctAnswer && index === currIndex) {
-                        color = "red";
+                // highlight correct answer
+                if (highlightAnswer) {
+                    if (choiceMarker === correctAnswer) {
+                        color = "green";
                         isInverse = true;
                     }
                 }
@@ -54,85 +154,6 @@ function MCList({
                     </Box>
                 );
             })}
-        </Box>
-    );
-}
-
-export function MultipleChoice({
-    question,
-}: {
-    question: MC;
-}): React.ReactElement {
-    const [currIndex, setCurrIndex] = useState<number>(0);
-    const [isFlashing, setIsFlashing] = useState<boolean>(false);
-    const [highlightCorrect, setHighlightCorrect] = useState<boolean>(false);
-
-    function flashCorrectAnswer(): void {
-        setIsFlashing(true);
-
-        // setInterval only has access to the global scoped variables at the time
-        // of instantiation of the Interval and given that React is immutable,
-        // those old highlightCorrect values that would be otherwise garbage
-        // collected because they are no longer relevant would still be used by
-        // setInterval and thus no changes would be rendered.  This is the
-        // reason for creating a locally scoped variable that is NOT part of
-        // React's useState hook
-        let localScopedHl: boolean = false;
-
-        const intervalID = setInterval(() => {
-            localScopedHl = !localScopedHl;
-            setHighlightCorrect(localScopedHl);
-        }, 500);
-
-        setTimeout(() => {
-            clearInterval(intervalID);
-            setIsFlashing(false);
-            setHighlightCorrect(false);
-        }, 2000);
-    }
-
-    useInput((input, key) => {
-        if (input === "j" || key.downArrow) {
-            if (currIndex === question.choices.length - 1 || isFlashing) {
-                return;
-            } else {
-                setCurrIndex(currIndex + 1);
-            }
-        }
-
-        if (input === "k" || key.upArrow) {
-            if (currIndex === 0 || isFlashing) {
-                return;
-            } else {
-                setCurrIndex(currIndex - 1);
-            }
-        }
-
-        if (input === "n") {
-            setCurrIndex(0);
-        }
-
-        if (key.return) {
-            if (!isFlashing) {
-                flashCorrectAnswer();
-            }
-        }
-    });
-
-    return (
-        <Box justifyContent="center" flexDirection="column">
-            <Box alignSelf="center">
-                <Text color="yellow" dimColor>
-                    {question.q}
-                </Text>
-            </Box>
-            <HorizontalLine />
-            <MCList
-                choices={question.choices}
-                currIndex={currIndex}
-                highlightCorrect={highlightCorrect}
-                correctAnswer={question.a}
-            />
         </Box>
     );
 }
